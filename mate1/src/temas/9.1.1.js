b3321427e9dbc1b5a7f17f6d37af971ddb127513
robemorin <robertomorin2@gmail.com>
2025-08-19 22:20:01 -0600

    Update 9.1.1.js

--- mate1/src/temas/9.1.1.js
+++ mate1/src/temas/9.1.1.js
@@ -2,7 +2,7 @@ import * as tlacu from 'https://robemorin.github.io/tlacuache/src/tlacuache-modu
 import 'https://robemorin.github.io/tlacuache/src/tlacuache-elements.js'
 import {desencriptar, encriptar, generarCodigo} from '../r2p_core.js'
 export function name() {
-  return 'Denavit-Hartenberg II';
+  return 'Puntos en el espacio';
 }
 
 export function tipo() {
