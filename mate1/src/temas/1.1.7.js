5e2c77846131f2c133bcbc4560f24f2643669184
robemorin <robertomorin2@gmail.com>
2026-03-24 07:09:13 -0600

    2026-03-24

--- mate1/src/temas/1.1.7.js
+++ mate1/src/temas/1.1.7.js
@@ -1,26 +1,26 @@
 import * as tlacu from 'https://robemorin.github.io/tlacuache/src/tlacuache-modulo.mjs';
 import 'https://robemorin.github.io/tlacuache/src/tlacuache-elements.js'
-import {desencriptar, encriptar, generarCodigo} from '../r2p_core.js'
+import { desencriptar, encriptar, generarCodigo } from '../r2p_core.js'
 export function name() {
-  return 'Gráfica de la línea forma general';
+  return 'Ecuación de la línea en la forma general a partir de su gráfica';
 }
 
 export function tipo() {
   return 3;
 }
 
-export async function pregunta(i, totalPreguntas, esImprimible=false) {
-  try{
-    let A=[Math.ceil(Math.random()*8-4),Math.ceil(Math.random()*8-4)], B
-    do{
-      B = [Math.ceil(Math.random()*8-4),Math.ceil(Math.random()*8-4)]
-    }while( (A[0] == B[0]) || (A[1] == B[1]) )
-    let m=[(B[1]-A[1]),(B[0]-A[0])]
+export async function pregunta(i, totalPreguntas, esImprimible = false) {
+  try {
+    let A = [Math.ceil(Math.random() * 8 - 4), Math.ceil(Math.random() * 8 - 4)], B
+    do {
+      B = [Math.ceil(Math.random() * 8 - 4), Math.ceil(Math.random() * 8 - 4)]
+    } while ((A[0] == B[0]) || (A[1] == B[1]))
+    let m = [(B[1] - A[1]), (B[0] - A[0])]
     m = tlacu.simplify_frac(m)
 
 
 
-    const Pregunta = `
+    const Pregunta = `${i == 0 ? '<h2>Considere que en la ecuación $Ax+By+C=0$, $A>0$ y $A,B,C$ son números enteros primos relativos y $A$ es no negativo.</h2>' : ''}
       <div class="pregunta-abierta"  data-a="${A}" data-b="${B}" style="display: none;">
         <p>${i + 1}.- Obtenga la ecuación de la siguiente gráfica en la forma $Ax+By+C=0$. <span id="resultado_${i}" name="question"></span></p>
         <div id="applet_container_${i}" class="ggb-container"></div>
@@ -31,23 +31,23 @@ export async function pregunta(i, totalPreguntas, esImprimible=false) {
           </table>
       </div>
     `;
-    if(esImprimible){
-	const respuesta=`$${tempEcLine(A,m)}$`
-	return [Pregunta, respuesta]
-  }
-  
-    if (totalPreguntas-1 == i) render(null,totalPreguntas)
+    if (esImprimible) {
+      const respuesta = `$${tempEcLine(A, m)}$`
+      return [Pregunta, respuesta]
+    }
+
+    if (totalPreguntas - 1 == i) render(null, totalPreguntas)
     return Pregunta
-  
-    
-  }catch (error){
+
+
+  } catch (error) {
     console.error('Error al carga la pregunta:', error);
   }
 }
 
 export async function render(container, n, code) {
   console.log('Ejecutar render')
-   const material_id = "efdp7g27";
+  const material_id = "efdp7g27";
   window.ggbApps = [];
 
   for (let i = 0; i < n; i++) {
@@ -61,62 +61,62 @@ export async function render(container, n, code) {
         let pregunta = document.getElementsByClassName('pregunta-abierta')
         const A = pregunta[i].dataset.a.split(',').map(Number)
         const B = pregunta[i].dataset.b.split(',').map(Number)
-        const m=[(B[1]-A[1]),(B[0]-A[0])]
+        const m = [(B[1] - A[1]), (B[0] - A[0])]
         console.log(`${i}.- A:${pregunta[i].dataset.a} B:${pregunta[i].dataset.b} m:${m}`)
         window.ggbApps[i] = api;
-      api.evalCommand(`f(x) = (${m[0]}/${m[1]})*(x-(${A[0]}))+(${A[1]})`);
+        api.evalCommand(`f(x) = (${m[0]}/${m[1]})*(x-(${A[0]}))+(${A[1]})`);
 
       }
     };
     new GGBApplet(params, true).inject(`applet_container_${i}`);
   }
   console.log('vamos a publicar la revisión')
-  window.accionR2P = function(i) {
+  window.accionR2P = function (i) {
     const api = window.ggbApps[i];
     if (!api) return alert("Applet no listo");
 
     let totalPuntos = 1
     let puntos = 0
     let pregunta = document.getElementsByClassName('pregunta-abierta')
-    const mathFields= pregunta[i].getElementsByTagName('math-field')
+    const mathFields = pregunta[i].getElementsByTagName('math-field')
     const A = pregunta[i].dataset.a.split(',').map(Number)
     const B = pregunta[i].dataset.b.split(',').map(Number)
-    let m=[(B[1]-A[1]),(B[0]-A[0])]
+    let m = [(B[1] - A[1]), (B[0] - A[0])]
     m = tlacu.simplify_frac(m)
-    const respuestaC = tempEcLine(A,m)
+    const respuestaC = tempEcLine(A, m)
 
 
     const respuesta = mathFields[0].value
     console.log(`'${respuesta}' ? '${respuestaC}'`)
-    
-    if( respuestaC != respuesta){
+
+    if (respuestaC != respuesta) {
       mathFields[0].style.backgroundColor = "red";
-      return [puntos,totalPuntos]
-    } else{
+      return [puntos, totalPuntos]
+    } else {
       ++puntos
       mathFields[0].style.border = "solid 5px green";
     }
 
-    return [puntos,totalPuntos]
+    return [puntos, totalPuntos]
   };
 }
 
 
 
-						function tempEcLine(A,D){
-							if(D[0]<0){
-								D=[-D[0],D[1]]
-							}else{
-                D=[D[0],-D[1]]
-              }
-							let cadena = `${D[0]==0?'':(D[0]==1?'':D[0])+'x'}`
-							cadena += `${D[1]==0?'':(Math.abs(D[1])==1?
-														(D[1]<0?'-y':'+y'):
-														(D[1]<0?D[1]+'y':`+${D[1]}y`))
-							}`
-							const c = -(D[0]*A[0]+D[1]*A[1])
-							cadena += `${(c==0?'':(c<0?'-':'+')+Math.abs(c))}`
-							cadena += `=0`
-							
-							return cadena
-						}
+function tempEcLine(A, D) {
+  if (D[0] < 0) {
+    D = [-D[0], D[1]]
+  } else {
+    D = [D[0], -D[1]]
+  }
+  let cadena = `${D[0] == 0 ? '' : (D[0] == 1 ? '' : D[0]) + 'x'}`
+  cadena += `${D[1] == 0 ? '' : (Math.abs(D[1]) == 1 ?
+    (D[1] < 0 ? '-y' : '+y') :
+    (D[1] < 0 ? D[1] + 'y' : `+${D[1]}y`))
+    }`
+  const c = -(D[0] * A[0] + D[1] * A[1])
+  cadena += `${(c == 0 ? '' : (c < 0 ? '-' : '+') + Math.abs(c))}`
+  cadena += `=0`
+
+  return cadena
+}
--- mate1/src/temas/1.2.1.js
+++ mate1/src/temas/1.2.1.js
@@ -1,6 +1,6 @@
 import * as tlacu from 'https://robemorin.github.io/tlacuache/src/tlacuache-modulo.mjs';
 import 'https://robemorin.github.io/tlacuache/src/tlacuache-elements.js'
-import {desencriptar, encriptar, generarCodigo} from '../r2p_core.js'
+import { desencriptar, encriptar, generarCodigo } from '../r2p_core.js'
 
 export function name() {
   return 'Gráfica de la línea en forma general';
@@ -13,37 +13,37 @@ export function tipo() {
 function gcd(x, y) {
   x = Math.abs(x);
   y = Math.abs(y);
-  while(y) {
-      let t = y;
-      y = x % y;
-      x = t;
+  while (y) {
+    let t = y;
+    y = x % y;
+    x = t;
   }
   return x;
 }
 
 function tempEcLineGeneral(A, B, C) {
-    let cadena = "";
-    if (A === 1) cadena += "x";
-    else if (A > 1) cadena += A + "x";
+  let cadena = "";
+  if (A === 1) cadena += "x";
+  else if (A > 1) cadena += A + "x";
 
-    if (B === 1) cadena += "+y";
-    else if (B === -1) cadena += "-y";
-    else if (B > 1) cadena += "+" + B + "y";
-    else if (B < -1) cadena += B + "y";
+  if (B === 1) cadena += "+y";
+  else if (B === -1) cadena += "-y";
+  else if (B > 1) cadena += "+" + B + "y";
+  else if (B < -1) cadena += B + "y";
 
-    if (C > 0) cadena += "+" + C;
-    else if (C < 0) cadena += C;
+  if (C > 0) cadena += "+" + C;
+  else if (C < 0) cadena += C;
 
-    return cadena + "=0";
+  return cadena + "=0";
 }
 
 export async function pregunta(i, code) {
-  try{
+  try {
     let A = [Math.ceil(Math.random() * 10 - 5), Math.ceil(Math.random() * 10 - 5)], B;
-    do{
-        B = [Math.ceil(Math.random() * 10 - 5), Math.ceil(Math.random() * 10 - 5)]
-    }while( (A[0] == B[0]) || (A[1] == B[1]) )
-    
+    do {
+      B = [Math.ceil(Math.random() * 10 - 5), Math.ceil(Math.random() * 10 - 5)]
+    } while ((A[0] == B[0]) || (A[1] == B[1]))
+
     let dy = B[1] - A[1];
     let dx = B[0] - A[0];
 
@@ -52,25 +52,25 @@ export async function pregunta(i, code) {
     let c = dx * A[1] - dy * A[0];
 
     if (a < 0) {
-        a = -a;
-        b = -b;
-        c = -c;
+      a = -a;
+      b = -b;
+      c = -c;
     }
 
     let divisor = gcd(gcd(a, b), c);
     a = a / divisor;
     b = b / divisor;
     c = c / divisor;
-    
+
     const linea = tempEcLineGeneral(a, b, c);
-    
+
     return `
       <div class="pregunta-geogebra"  data-a="${A}" data-b="${B}" style="display: none;">
         <p>${i + 1}.- Mueva los puntos $A$ y $B$ para obtener la gráfica de la línea $${linea}$. <span id="resultado_${i}" name="question"></span></p>
         <div id="applet_container_${i}" class="ggb-container"></div>
       </div>
     `;
-  }catch (error){
+  } catch (error) {
     console.error('Error al carga la pregunta:', error);
   }
 }
@@ -94,7 +94,7 @@ export async function renderGeoGebra(container, n, code) {
   }
 
   // 🔁 Registrar función específica del tema
-  window.accionGeoGebra = function(i) {
+  window.accionGeoGebra = function (i) {
     const api = window.ggbApps[i];
     let pregunta = document.getElementsByClassName('pregunta-geogebra')[i]
     if (!api) return alert("Applet no listo");
@@ -108,11 +108,11 @@ export async function renderGeoGebra(container, n, code) {
 
     const A = pregunta.dataset.a.split(',').map(Number)
     const B = pregunta.dataset.b.split(',').map(Number)
-    let m=(B[1]-A[1])/( B[0]-A[0] )
-    let b=B[1]-m*B[0]
+    let m = (B[1] - A[1]) / (B[0] - A[0])
+    let b = B[1] - m * B[0]
 
-    if(ax != bx && Math.abs(ay - (m*ax+b)) < 0.01 && Math.abs(by - (m*bx+b)) < 0.01 )   return true
+    if (ax != bx && Math.abs(ay - (m * ax + b)) < 0.01 && Math.abs(by - (m * bx + b)) < 0.01) return true
     return false
-    
+
   };
 }
